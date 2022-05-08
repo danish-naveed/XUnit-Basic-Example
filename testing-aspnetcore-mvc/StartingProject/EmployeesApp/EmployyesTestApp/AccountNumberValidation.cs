0fe68263a1f0502cdfcfefc63c47a78255ac7e77
danish-naveed <danish.naveed@tkxel.io>
2022-05-08 15:04:16 +0500

    first commit

---
+++ testing-aspnetcore-mvc/StartingProject/EmployeesApp/EmployyesTestApp/AccountNumberValidation.cs
@@ -0,0 +1,37 @@
+﻿using EmployeesApp.Validation;
+using System.Collections.Generic;
+using Xunit;
+
+namespace EmployyesTestApp
+{
+    public class AccountNumberValidationTests
+    {
+        private readonly AccountNumberValidation _validation;
+
+        public AccountNumberValidationTests() => _validation = new AccountNumberValidation();
+
+        [Theory]
+        [InlineData("13-4543234576-23")]
+        [InlineData("1334-4543234576-23")]
+        public void IsValid_ValidAccountNumberFirstPart_ReturnsFalse(string accountNumber)
+        {
+            Assert.False(_validation.IsValid(accountNumber));
+        }
+
+        [Theory]
+        [InlineData("13-543234576-23")]
+        [InlineData("13-95432345763-23")]
+        public void IsValid_ValidAccountNumberMiddlePart_ReturnsFalse(string accountNumber)
+        {
+            Assert.False(_validation.IsValid(accountNumber));
+        }
+
+        [Theory]
+        [InlineData("13-4543234576-3")]
+        [InlineData("13-4543234576-2333")]
+        public void IsValid_ValidAccountNumberLastPart_ReturnsFalse(string accountNumber)
+        {
+            Assert.False(_validation.IsValid(accountNumber));
+        }
+    }
+}
